b24978977c8e75cf951b51319b35817fb2e717cf
Joaquín Ossandón <joaquinossandong@gmail.com>
2026-08-25 20:57:23 -0400

    fix: simplify CORS configuration by removing origin specification

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const { sequelize } = require("./src/config/database.js")
 const e = require("express")
 const app = express()
 
-app.use(cors({
-    origin: ["*"]
-}))
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
